670543991915e3d596bc06fcc169b0afb5a19943
Anuj  Babu <119038654+AnujBabu@users.noreply.github.com>
2023-07-19 10:17:12 +0530

    Add files via upload

---
+++ Anuj/object.js
@@ -0,0 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var person = {
+    name: 'Anuj ',
+    age: 23,
+    email: "anujbabu639797@gmail.com"
+};
+var User = {
+    name: 'Sasu',
+    age: 22,
+    email: "sasmitagiri266@gmail.com"
+};
+console.log(User.age);
